7501b06ce450c055620fb62a949dcca83eba69fb
GregNasir <63434657+GregNasir@users.noreply.github.com>
2023-04-20 17:56:12 -0400

    created varibles with list of expected characters

--- script.js
+++ script.js
@@ -2,15 +2,15 @@
 // Get references to the #generate element
 const generateBtn = document.querySelector("#generate");
 
-const uppercaseABC = "ABCDEFGHIJKLMNOPQRSTUVWXZ";
-const lowercaseABC ="abcdefghijklmnopqrstuvwxyz";
-const specialChar ="!@#$%^&*()?.<\>|=+:;,[-_]";
-const numeric ="0123456789";
+const uppercaseABC = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Z"];
+const lowercaseABC = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const specialChar = ["!","@","#","$","%","^","&","*","()","?",".","<","\",">","|",'=',"+",':',';',",",'[',"-",'_"];
+const numeric = ["0","1","2","3","4","5","6","7","8","9"];
 // const multiSelect =[];
 
-// const passwordLength = Number(prompt("How many character in password? Must be between 8 and 128)"));
+const passwordLength = Number(prompt("How many character in password? Must be between 8 and 128)"));
 
-// const passwordCharType = confirm("Do you want to  include lowercase, uppercase, numeric, and/or special characters?");
+const passwordCharType = confirm("Do you want to  include lowercase, uppercase, numeric, and/or special characters?");
 
 
 
